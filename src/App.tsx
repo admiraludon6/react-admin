bbd90b666cd661384f27f970904206ddaa19f9b5
Ahmad Miqdaad <ahmad.miqdaad@kalsym.com>
2023-02-16 12:38:59 +0800

    Create graph pages & components
    - Add BarChart, GeographyChart, LineChart & PieChart
    - Add Bar Page, FAQ Page, Geography Page, Line Page & Pie Page

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,12 @@ import Dashboard from './pages/dashboard';
 import Team from './pages/team';
 import Invoices from './pages/invoices';
 import Contacts from './pages/contacts';
-// import Bar from './pages/bar/Bar';
+import Bar from './pages/bar';
 import Form from './pages/form';
-// import Line from './pages/line/Line';
-// import Pie from './pages/pie/Pie';
-// import FAQ from './pages/faq/FAQ';
-// import Geography from './pages/geography/Geography';
+import Line from './pages/line';
+import Pie from './pages/pie';
+import FAQ from './pages/faq';
+import Geography from './pages/geography';
 import Calendar from './pages/calendar';
 
 function App() {
@@ -32,11 +32,11 @@ function App() {
               <Route path="/invoices" element={<Invoices/>}/>
               <Route path="/contacts" element={<Contacts/>}/>
               <Route path="/form" element={<Form/>}/>
-              {/* <Route path="/bar" element={<Bar/>}/> */}
-              {/* <Route path="/pie" element={<Pie/>}/> */}
-              {/* <Route path="/line" element={<Line/>}/> */}
-              {/* <Route path="/faq" element={<FAQ/>}/> */}
-              {/* <Route path="/geography" element={<Geography/>}/> */}
+              <Route path="/bar" element={<Bar/>}/>
+              <Route path="/pie" element={<Pie/>}/>
+              <Route path="/line" element={<Line/>}/>
+              <Route path="/faq" element={<FAQ/>}/>
+              <Route path="/geography" element={<Geography/>}/>
               <Route path="/calendar" element={<Calendar/>}/>
             </Routes>
           </main>
--- src/layouts/global/Sidebar.tsx
+++ src/layouts/global/Sidebar.tsx
@@ -188,8 +188,8 @@ const Sidebar = () => {
                             selected={selected} 
                             setSelected={setSelected} />
                         <Item 
-                            title={"Geograhpy Chart"} 
-                            to={"/geograhpy"}  
+                            title={"Geography Chart"} 
+                            to={"/geography"}  
                             icon={<MapOutlinedIcon/>} 
                             selected={selected} 
                             setSelected={setSelected} />
